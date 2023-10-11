e45cdbf01d0887295dadf48eb5ac942e67fe5db3
Jarrett D Sauceman <89327514+jdsauceman21@users.noreply.github.com>
2023-10-11 15:27:41 -0400

    moved js files to component folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import List from './List';
-import withListLoading from './withListLoading';
+import List from './Component/List';
+import withListLoading from './Component/withListLoading';
 
 function App() {
   const ListLoading = withListLoading(List);
